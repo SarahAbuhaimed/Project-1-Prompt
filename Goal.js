9db10796d38c85c888c2d38f2e6016b70d9bb0ee
sarahAbuhaimed <sar-abu@hotmail.com>
2019-03-11 16:02:43 +0300

    check winer done

--- Goal.js
+++ Goal.js
@@ -1,3 +1,4 @@
+//drag and drop img of football tems inside plyer div
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -9,39 +10,40 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
-var winCase ={
-    1:[0,1,2],
-    2:[3,4,5],
-    3:[6,7,8],
-    4:[2,4,6],
-    5:[2,5,8],
-    6:[1,4,7],
-    7:[0,4,8],
-    8:[0,3,6]
+  //object with all cases plyer can win by
+  var winCase ={
+      1:["square0","square1","square2"],
+    2:["square3","square4","square5"],
+    3:["square6","square7","square8"],
+    4:["square2","square4","square6"],
+   5: ["square2","square5","square8"],
+    6:["square1","square4","square7"],
+    7:["square0","square4","square8"],
+    8:["square0","square3","square6"]
 }
-var currentPlayer='';
-var currentScore = 0;
-var moves= 1;
-var player1= '';
-var player2= '';
-var win = 3;
-var finalScore = [0,0];
+//plyers variable store array of divs selected
+var player1=[];
+var player2=[];
+// var currentPlayer=0;
+// var currentScore = 0;
+// var finalScore = [0,0];
+//to store sign x and O
 var sign = true;
-
+//function for button which reload page 
 $('.buttonNew').on("click", function (event) {
         location.reload();
         currentScore = 0;
         currentPlayer = 0;
         finalScore = [0,0];
-         win = 3;
+      
     });
-       // highlight the mouseover target
+       // highlight the mouseover divs
     $('.square').on("mouseover", function (event) {
      
         $(this).addClass("black");
     
     });
-        // removw highlight the mouseover target
+        // removw highlight the mouseover divs
 $('.square').on("mouseout", function (event) {
 
     $(this).removeClass("black");
@@ -50,14 +52,44 @@ $('.square').on("mouseout", function (event) {
 //on click boxs to change from x to o
 $('.square').one("click", function (event) {
        $(this).off("mouseout");
-        // checkAll(this)
 if (sign == true){
     $(this).text("X");
     sign=!sign;
+ player1.push(event.target.id);
+ //
+console.log(player1);
 }else {
     $(this).text("O");
     sign=!sign;
-}
+   player2.push(event.target.id);
+   //event.target.id
+   console.log(player2);
 
-     })
-     
+  }  
+  checkWinner()
+})
+
+//fuction to check if player1 or plyer2 is win
+ function checkWinner() {
+     var noWon = true;
+    for (var i = 1; i < 9; i++) {
+        if (player1.includes(winCase[i][0]) && player1.includes(winCase[i][1]) && player1.includes(winCase[i][2])) {
+            noWon = false;
+            alert("Win")
+        } else if (player2.includes(winCase[i][0]) && player2.includes(winCase[i][1]) && player2.includes(winCase[i][2])) {
+            noWon = false;
+            alert("Win player 2")  
+        }
+        //if no one wins 
+        if (player1.length==5 || player1.length==5 ) {
+            if(noWon == true){
+                alert("no one win")
+                return;
+            }
+        }
+        } 
+    }
+
+
+
+
